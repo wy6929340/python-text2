ce236467437e792ed0bfc64cd63716012135ef72
wy6929340 <ly00558@hotmail.com>
2021-07-08 16:10:07 +0800

    update read.pt

--- read.py
+++ read.py
@@ -7,7 +7,10 @@ with open("reviews.txt", "r") as f:
         data.append(line)
         count +=1
         if count % 1000 == 0:
-            print(len(data)) 
+            print(len(data))
+#print(data[0]) 
+
+
 for d in data:
     len_total =len_total + len(d)
     len_num = len_total / len(data)
@@ -17,3 +20,47 @@ for d in data:
     if len(d) < 100:
         new_data.append(d)
 print(len(new_data))
+
+good_data = []
+for d in data:
+    if "good" in d:
+        good_data.append(d)
+print("一共有", len(good_data), "比留言")
+
+good = [d for d in data if "good" in d ]
+
+print(len(good))
+
+#文字記數
+wc = {}
+
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1 
+        else:
+            wc[word] = 1
+            
+    
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+while True:
+    k = input("請輸入想要查詢的字:")
+    if k == "q":
+        break
+    if k in wc:
+        print(k,"出現過", wc[k])
+    else:
+        print(k, "沒在字典裡")
+print("感謝使用")
+
+
+
+
+
+
+
+
